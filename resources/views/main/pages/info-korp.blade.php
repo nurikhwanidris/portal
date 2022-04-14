2cd3838f3e405ca3afc8dfc00f9775ad90667b6c
Azzurri <sarawirda@gmail.com>
2022-04-14 12:57:45 +0800

    page title

--- resources/views/main/pages/info-korp.blade.php
+++ resources/views/main/pages/info-korp.blade.php
@@ -13,7 +13,6 @@
 
     <!-- CSS & Script -->
     @include('main.layouts.partials.headerlink')
-
     @livewireStyles
 </head>
 
@@ -25,10 +24,8 @@
 
         <div role="main" class="main">
             <!-- Title Section - START -->
-            @include(
-                'main.layouts.partials.pages.info-korp.info-korp-title'
-            )
-            <!-- Title Section - END -->
+				@livewire('title.page-title')
+				<!-- Title Section - END -->
 
             <!-- Menu - START -->
             @include('main.layouts.partials.switcher')
@@ -36,17 +33,6 @@
 
 
             <div class="container" style="margin-bottom: 100px;">
-                <!-- Breadcrumb -->
-                <div class="row">
-                    <h5 class="text-1 font-weight-bolder">
-                        Laman Utama > Info Korporat > Mengenai Kami
-                    </h5>
-                </div>
-                <div class="row">
-                    <h2 class="font-weight-normal text-6">
-                        <strong class="text-title-strong">Mengenai</strong> JUPEM
-                    </h2>
-                </div>
                 <div class="row">
                     <!-- Icon Row - START -->
                     <div class="featured-boxes featured-boxes-style-3 featured-boxes-flat">
