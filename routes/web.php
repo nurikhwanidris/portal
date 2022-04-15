9cb95370b1d3390fe38162439b3b6442330fd0b4
Nur Ikhwan Idris <nurikhwanidris@gmail.com>
2022-04-15 09:42:42 +0800

    renamed the route

--- routes/web.php
+++ routes/web.php
@@ -25,13 +25,8 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Admin Route
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
 
 // SPSM CMS route
