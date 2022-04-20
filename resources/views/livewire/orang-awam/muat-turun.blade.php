2c7ba05af15f866b77e689ac6d675127ff4ad902
Azzurri <sarawirda@gmail.com>
2022-04-20 15:00:05 +0800

    data terbuka+muat turun

--- resources/views/livewire/orang-awam/muat-turun.blade.php
+++ resources/views/livewire/orang-awam/muat-turun.blade.php
@@ -1,3 +1,41 @@
 <div>
-    {{-- The Master doesn't talk, he acts. --}}
+    <div>
+        <div class="row">
+            <div class="col">
+                <div class="text-title-semasa-11" style="padding-bottom: 20px;">Muat turun</div>
+            </div>
+        </div>
+
+        <!-- SUB TITLE - START-->
+        <h2 class="font-weight-normal text-6 mb-3">Borang Permohonan Melakukan Pekerjaan Luar</h2>
+        <!-- SUB TITLE - END-->
+
+        <!--  Filtering -->
+        <div class="row kotak-filter">
+            <div class="form-group col-lg-12">
+                <label class="form-label mb-1 text-2">Select</label>
+                <div class="custom-select-1">
+                    <select class="form-select form-control  border-color-quaernary h-auto" name="dd-nama" required>
+                        <option value="">Choose...</option>
+                        <option value="1">Borang Permohonan Melakukan Pekerjaan Luar</option>
+                        <option value="2">Borang Change Request (Portal)</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+
+        <!-- LIST TEXT - START -->
+        <div class="row align-items-center py-5 appear-animation" data-appear-animation="fadeInRightShorter" style="padding-top:0 !important;">
+            <div class="col-md-12 pe-md-5 mb-5 mb-md-0">
+                <ul class="list list-icons list-icons-style-3 list-tertiary list-icons-sm">
+                    <li><i class="fas fa-check"></i><a href="#">1</a></li>
+                    <li><i class="fas fa-check"></i><a href="#">2</a></li>
+                    <li><i class="fas fa-check"></i><a href="#">3</a></li>
+                </ul>
+            </div>
+        </div>
+        <!-- LIST TEXT - END -->
+
+
+    </div>
 </div>
