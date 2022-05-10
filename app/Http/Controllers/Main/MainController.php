d012db2675d15c149ae5951c990f1e435cd6fde9
Nur Ikhwan Idris <nurikhwanidris@gmail.com>
2022-05-10 12:16:16 +0800

    changed the main controller

--- app/Http/Controllers/Main/MainController.php
+++ app/Http/Controllers/Main/MainController.php
@@ -11,90 +11,93 @@ class MainController extends Controller
     // Index controller
     public function index()
     {
-        return view('main.index',[
+        return view('main.index', [
             'counter' => Visitor::whereMonth('date', '=', now()->format('m'))->get()->count(),
             'activity' => DB::table('logs')->select('log_date')->orderBy('log_date', 'desc')->first()
         ]);
     }
-
     // Info Korporat controller
     public function infoKorp()
     {
-        return view('main.pages.info-korp',[
+        return view('main.pages.info-korp', [
             'counter' => Visitor::whereMonth('date', '=', now()->format('m'))->get()->count(),
             'activity' => DB::table('logs')->select('log_date')->orderBy('log_date', 'desc')->first()
         ]);
     }
-
     // Orang Awam controller`
     public function orangAwam()
     {
-        return view('main.pages.orang-awam',[
+        return view('main.pages.orang-awam', [
             'counter' => Visitor::whereMonth('date', '=', now()->format('m'))->get()->count(),
             'activity' => DB::table('logs')->select('log_date')->orderBy('log_date', 'desc')->first()
         ]);
     }
-
     // Page Element Controller
     public function pageElement()
     {
-        return view('main.pages.page-element',[
+        return view('main.pages.page-element', [
             'counter' => Visitor::whereMonth('date', '=', now()->format('m'))->get()->count(),
             'activity' => DB::table('logs')->select('log_date')->orderBy('log_date', 'desc')->first()
         ]);
     }
-
     // Warga Jupem Controller
     public function wargaJupem()
     {
-        return view('main.pages.warga-jupem',[
+        return view('main.pages.warga-jupem', [
             'counter' => Visitor::whereMonth('date', '=', now()->format('m'))->get()->count(),
             'activity' => DB::table('logs')->select('log_date')->orderBy('log_date', 'desc')->first()
         ]);
     }
-
     // FAQ Controller
     public function faq()
     {
-        return view('main.pages.faq',[
+        return view('main.pages.faq', [
             'counter' => Visitor::whereMonth('date', '=', now()->format('m'))->get()->count(),
             'activity' => DB::table('logs')->select('log_date')->orderBy('log_date', 'desc')->first()
         ]);
     }
-
     // Sitemap Controller
     public function sitemap()
     {
-        return view('main.pages.sitemap',[
+        return view('main.pages.sitemap', [
             'counter' => Visitor::whereMonth('date', '=', now()->format('m'))->get()->count(),
             'activity' => DB::table('logs')->select('log_date')->orderBy('log_date', 'desc')->first()
         ]);
     }
-
-    // Direktori Pegawai Controller
-    public function direktoriPegawai()
+    // DasarPrivasi Controller
+    public function dasarPrivasi()
     {
-        return view('main.pages.direktori-pegawai',[
+        return view('main.pages.dasar-privasi', [
             'counter' => Visitor::whereMonth('date', '=', now()->format('m'))->get()->count(),
-            'activity' => DB::table('logs')->select('log_date')->orderBy('log_date', 'desc')->first()
+            'activity' => DB::table('logs')->select('log_date')->orderBy('log_date', 'desc')->first(),
+            'k' => 'jjjj'
         ]);
     }
-
-    // Tugas Peranan Controller
-    public function tugasPeranan()
+    // DasarKeselamatan Controller
+    public function dasarKeselamatan()
     {
-        return view('main.pages.tugas-peranan',[
+        return view('main.pages.dasar-keselamatan', [
             'counter' => Visitor::whereMonth('date', '=', now()->format('m'))->get()->count(),
-            'activity' => DB::table('logs')->select('log_date')->orderBy('log_date', 'desc')->first()
+            'activity' => DB::table('logs')->select('log_date')->orderBy('log_date', 'desc')->first(),
+            'k' => 'jjjj'
         ]);
     }
-
-    // Visi Misi Controller
-    public function visiMisi()
+    // Penafian Controller
+    public function penafian()
     {
-        return view('main.pages.visi-misi',[
+        return view('main.pages.penafian', [
             'counter' => Visitor::whereMonth('date', '=', now()->format('m'))->get()->count(),
-            'activity' => DB::table('logs')->select('log_date')->orderBy('log_date', 'desc')->first()
+            'activity' => DB::table('logs')->select('log_date')->orderBy('log_date', 'desc')->first(),
+            'k' => 'jjjj'
+        ]);
+    }
+    // Penafian Controller
+    public function infoSemasa()
+    {
+        return view('main.pages.info-semasa', [
+            'counter' => Visitor::whereMonth('date', '=', now()->format('m'))->get()->count(),
+            'activity' => DB::table('logs')->select('log_date')->orderBy('log_date', 'desc')->first(),
+            'k' => 'jjjj'
         ]);
     }
 }
