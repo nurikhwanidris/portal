7edbc48e7e4e31f2fbabb8506c41cdde8584b96d
Nur Ikhwan Idris <35907238+nurikhwanidris@users.noreply.github.com>
2022-06-19 23:29:40 +0800

    Update TenderSebutharga.php

--- app/Http/Livewire/OrangAwam/TenderSebutharga.php
+++ app/Http/Livewire/OrangAwam/TenderSebutharga.php
@@ -5,16 +5,48 @@ namespace App\Http\Livewire\OrangAwam;
 use App\Models\Quote;
 use App\Models\Tender;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class TenderSebutharga extends Component
 {
-    // public $tajuk = 'Tajuk';
+    use WithPagination;
+
+    protected $paginationTheme = 'bootstrap';
+
+    public $searchTender = '';
+    public $searchQuote = '';
+
+    public $selectTenderYear;
+    public $selectQuoteYear;
+
+    public function updatingSearchTender()
+    {
+        $this->resetPage();
+    }
 
     public function render()
     {
-        return view('livewire.orang-awam.tender-sebutharga',[
-            'tenders' => Tender::all(),
-            'quotes' => Quote::all()
+        // Tender
+        if ($this->searchTender != '' || $this->selectTenderYear != '') {
+            if ($this->searchTender != '')
+                $queryTenders = Tender::where('title_my', 'like', '%' . $this->searchTender . '%')->orderBy('created_at', 'desc')->paginate(5);
+            else if ($this->selectTenderYear != '')
+                $queryTenders = Tender::whereYear('created_at', $this->selectTenderYear)->orderBy('created_at', 'desc')->paginate(5);
+        } else
+            $queryTenders = Tender::orderBy('created_at', 'desc')->paginate(5);
+
+        // Quote
+        if ($this->searchQuote != '' || $this->selectQuoteYear != '') {
+            if ($this->searchQuote != '')
+                $queryQuote = Quote::where('title_my', 'like', '%' . $this->searchQuote . '%')->orderBy('created_at', 'desc')->paginate(5);
+            else if ($this->selectQuoteYear != '')
+                $queryQuote = Quote::whereYear('created_at', $this->selectQuoteYear)->orderBy('created_at', 'desc')->paginate(5);
+        } else
+            $queryQuote = Quote::orderBy('created_at', 'desc')->paginate(5);
+
+        return view('livewire.orang-awam.tender-sebutharga', [
+            'tenders' => $queryTenders,
+            'quotes' => $queryQuote,
         ]);
     }
 }
