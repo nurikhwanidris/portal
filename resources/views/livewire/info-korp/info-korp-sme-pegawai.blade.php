c4e649f66fa1a20a46db3df925968c9892b8c747
Nur Ikhwan Idris <nurikhwanidris@gmail.com>
2022-07-13 13:17:58 +0800

    Changed a fiew things

--- resources/views/livewire/info-korp/info-korp-sme-pegawai.blade.php
+++ resources/views/livewire/info-korp/info-korp-sme-pegawai.blade.php
@@ -8,12 +8,12 @@
         <!-- Maklumat Pegawai - START -->
         <div class="row pb-0 pt-3 mb-5 appear-animation" data-appear-animation="fadeInUpShorter"
             data-appear-animation-delay="200">
-            <div class="col-sm-6 col-lg-4 mb-4 mb-lg-0">
+            <div class="col-sm-6 col-lg-6 mb-4 mb-lg-0">
                 <span class="thumb-info thumb-info-hide-wrapper-bg thumb-info-no-zoom">
                     <span class="thumb-info-wrapper">
                         <a href="#">
-                            <img src="{{ asset('storage/upload/img/pegawai/dafb5-sr-hazri-g.jpg') }}" class="img-fluid"
-                                alt="">
+                            <img src="{{ asset('storage/upload/img/pegawai/dafb5-sr-hazri-g.jpg') }}"
+                                class="img-fluid" alt="">
                         </a>
                     </span>
                     <span class="thumb-info-caption">
@@ -48,6 +48,317 @@
                                 <i class="fas fa-phone top-6"></i>
                                 <strong>Tel:</strong><br>
                                 03-2617 0831
+                            </li>
+                        </ul>
+                    </span>
+                </span>
+            </div>
+            <div class="col-sm-6 col-lg-6 mb-4 mb-lg-0">
+                <span class="thumb-info thumb-info-hide-wrapper-bg thumb-info-no-zoom">
+                    <span class="thumb-info-wrapper">
+                        <a href="#">
+                            <img src="{{ asset('storage/upload/img/pegawai/8f047-sr-looi-kam-seng.jpg') }}"
+                                class="img-fluid" alt="" style="width: 400px !important">
+                        </a>
+                    </span>
+                    <span class="thumb-info-caption">
+                        <ul class="list list-icons list-icons-style-2 list-icons-sm mt-2 appear-animation ul-kite"
+                            data-appear-animation="maskUp" data-appear-animation-delay="300">
+                            <h4 class="font-weight-bold mb-2 size-text-standard">Sr LOOI KAM SENG</h4>
+                            <h2 class="font-weight-bold mb-0 pt-0 mt-0 size-text-standard-2">
+                                Pengarah Ukur Seksyen Perundangan Kadaster, Bahagian Kadaster
+                            </h2>
+                            <li class="li-kite">
+                                <i class="fas fa-envelope top-6"></i>
+                                <strong>Gred:</strong><br>
+                                Juruukur Gred Khas C (KUP) (SME)
+                            </li>
+                            <li class="li-kite">
+                                <i class="fas fa-envelope top-6"></i>
+                                <strong>Bidang Kepakaran:</strong><br>
+                                SME Kadaster (Ukuran Kadaster)
+                            </li>
+                            <li class="li-kite">
+                                <i class="fas fa-envelope top-6"></i>
+                                <strong>Tahap:</strong><br>
+                                4
+                            </li>
+                            <li class="li-kite">
+                                <i class="fas fa-envelope top-6"></i>
+                                <strong>Email:</strong><br>
+                                <a href="mailto:?">looi@jupem.gov.my</a>
+                            </li>
+                            <li class="li-kite">
+                                <i class="fas fa-phone top-6"></i>
+                                <strong>Tel:</strong><br>
+                                03-2617 0870
+                            </li>
+                        </ul>
+                    </span>
+                </span>
+            </div>
+            <div class="col-sm-6 col-lg-6 mb-4 mb-lg-0">
+                <span class="thumb-info thumb-info-hide-wrapper-bg thumb-info-no-zoom">
+                    <span class="thumb-info-wrapper">
+                        <a href="#">
+                            <img src="{{ asset('storage/upload/img/pegawai/54c8c-3.-sr-mohd-azua-bin-mohd-zain.jpg') }}"
+                                class="img-fluid" alt="" style="width: 400px !important">
+                        </a>
+                    </span>
+                    <span class="thumb-info-caption">
+                        <ul class="list list-icons list-icons-style-2 list-icons-sm mt-2 appear-animation ul-kite"
+                            data-appear-animation="maskUp" data-appear-animation-delay="300">
+                            <h4 class="font-weight-bold mb-2 size-text-standard">Sr MOHD AZUA BIN MOHD ZAIN</h4>
+                            <h2 class="font-weight-bold mb-0 pt-0 mt-0 size-text-standard-2">
+                                Ketua Penolong Pengarah Ukur Bahagian Kadaster
+                            </h2>
+                            <li class="li-kite">
+                                <i class="fas fa-envelope top-6"></i>
+                                <strong>Gred:</strong><br>
+                                Juruukur Gred J52 (KUP) (SME)
+                            </li>
+                            <li class="li-kite">
+                                <i class="fas fa-envelope top-6"></i>
+                                <strong>Bidang Kepakaran:</strong><br>
+                                SME Kadaster (Ukuran Strata/ Stratum dan 3D Kadaster)
+                            </li>
+                            <li class="li-kite">
+                                <i class="fas fa-envelope top-6"></i>
+                                <strong>Tahap:</strong><br>
+                                4
+                            </li>
+                            <li class="li-kite">
+                                <i class="fas fa-envelope top-6"></i>
+                                <strong>Email:</strong><br>
+                                <a href="mailto:?">mohdazua.zain@jupem.gov.my</a>
+                            </li>
+                            <li class="li-kite">
+                                <i class="fas fa-phone top-6"></i>
+                                <strong>Tel:</strong><br>
+                                03 2617 0682
+                            </li>
+                        </ul>
+                    </span>
+                </span>
+            </div>
+            <div class="col-sm-6 col-lg-6 mb-4 mb-lg-0">
+                <span class="thumb-info thumb-info-hide-wrapper-bg thumb-info-no-zoom">
+                    <span class="thumb-info-wrapper">
+                        <a href="#">
+                            <img src="{{ asset('storage/upload/img/pegawai/3a258-3.-sr-jasmari-bin-jamaludin.jpg') }}"
+                                class="img-fluid" alt="" style="width: 400px !important">
+                        </a>
+                    </span>
+                    <span class="thumb-info-caption">
+                        <ul class="list list-icons list-icons-style-2 list-icons-sm mt-2 appear-animation ul-kite"
+                            data-appear-animation="maskUp" data-appear-animation-delay="300">
+                            <h4 class="font-weight-bold mb-2 size-text-standard">Sr JASMARI BIN JAMALUDIN</h4>
+                            <h2 class="font-weight-bold mb-0 pt-0 mt-0 size-text-standard-2">
+                                Timbalan Pengarah Ukur dan Pemetaan JUPEM Wilayah Persekutuan Kuala Lumpur/Putrajaya
+                            </h2>
+                            <li class="li-kite">
+                                <i class="fas fa-envelope top-6"></i>
+                                <strong>Gred:</strong><br>
+                                Juruukur Gred J48 (KUP) (SME)
+                            </li>
+                            <li class="li-kite">
+                                <i class="fas fa-envelope top-6"></i>
+                                <strong>Bidang Kepakaran:</strong><br>
+                                SME Kadaster (Penawanan Data Terrestrial dan 3D Modelling)
+                            </li>
+                            <li class="li-kite">
+                                <i class="fas fa-envelope top-6"></i>
+                                <strong>Tahap:</strong><br>
+                                1
+                            </li>
+                            <li class="li-kite">
+                                <i class="fas fa-envelope top-6"></i>
+                                <strong>Email:</strong><br>
+                                <a href="mailto:?">jasmari@jupem.gov.my</a>
+                            </li>
+                            <li class="li-kite">
+                                <i class="fas fa-phone top-6"></i>
+                                <strong>Tel:</strong><br>
+                                03-2617 0662
+                            </li>
+                        </ul>
+                    </span>
+                </span>
+            </div>
+            <div class="col-sm-6 col-lg-6 mb-4 mb-lg-0">
+                <span class="thumb-info thumb-info-hide-wrapper-bg thumb-info-no-zoom">
+                    <span class="thumb-info-wrapper">
+                        <a href="#">
+                            <img src="{{ asset('storage/upload/img/pegawai/d6125-sr-nur-zurairah-binti-abdul-halim.jpg') }}"
+                                class="img-fluid" alt="" style="width: 400px !important">
+                        </a>
+                    </span>
+                    <span class="thumb-info-caption">
+                        <ul class="list list-icons list-icons-style-2 list-icons-sm mt-2 appear-animation ul-kite"
+                            data-appear-animation="maskUp" data-appear-animation-delay="300">
+                            <h4 class="font-weight-bold mb-2 size-text-standard">Sr NUR ZURAIRAH BINTI ABDUL HALIM</h4>
+                            <h2 class="font-weight-bold mb-0 pt-0 mt-0 size-text-standard-2">
+                                Penolong Pengarah Ukur Seksyen Naziran Kadaster, Bahagian Kadaster
+                            </h2>
+                            <li class="li-kite">
+                                <i class="fas fa-envelope top-6"></i>
+                                <strong>Gred:</strong><br>
+                                Juruukur Gred J48 (KUP) (SME)
+                            </li>
+                            <li class="li-kite">
+                                <i class="fas fa-envelope top-6"></i>
+                                <strong>Bidang Kepakaran:</strong><br>
+                                SME Kadaster (Penawanan Data Terrestrial dan 3D Modelling)
+                            </li>
+                            <li class="li-kite">
+                                <i class="fas fa-envelope top-6"></i>
+                                <strong>Tahap:</strong><br>
+                                1
+                            </li>
+                            <li class="li-kite">
+                                <i class="fas fa-envelope top-6"></i>
+                                <strong>Email:</strong><br>
+                                <a href="mailto:?">nurzurairah@jupem.gov.my</a>
+                            </li>
+                            <li class="li-kite">
+                                <i class="fas fa-phone top-6"></i>
+                                <strong>Tel:</strong><br>
+                                03-2617 0516
+                            </li>
+                        </ul>
+                    </span>
+                </span>
+            </div>
+            <div class="col-sm-6 col-lg-6 mb-4 mb-lg-0">
+                <span class="thumb-info thumb-info-hide-wrapper-bg thumb-info-no-zoom">
+                    <span class="thumb-info-wrapper">
+                        <a href="#">
+                            <img src="{{ asset('storage/upload/img/pegawai/5390c-yaep-wei-chien.jpg') }}"
+                                class="img-fluid" alt="" style="width: 400px !important">
+                        </a>
+                    </span>
+                    <span class="thumb-info-caption">
+                        <ul class="list list-icons list-icons-style-2 list-icons-sm mt-2 appear-animation ul-kite"
+                            data-appear-animation="maskUp" data-appear-animation-delay="300">
+                            <h4 class="font-weight-bold mb-2 size-text-standard">Sr YEAP WEI CHIEN</h4>
+                            <h2 class="font-weight-bold mb-0 pt-0 mt-0 size-text-standard-2">
+                                Penolong Pengarah Ukur Seksyen Pengurusan Maklumat Geospatial,Bahagian Geospatial
+                                Pertahanan
+                            </h2>
+                            <li class="li-kite">
+                                <i class="fas fa-envelope top-6"></i>
+                                <strong>Gred:</strong><br>
+                                Juruukur Gred J48 (KUP) (SME)
+                            </li>
+                            <li class="li-kite">
+                                <i class="fas fa-envelope top-6"></i>
+                                <strong>Bidang Kepakaran:</strong><br>
+                                SME Geodetik (Sistem Rujukan Koordinat)
+                            </li>
+                            <li class="li-kite">
+                                <i class="fas fa-envelope top-6"></i>
+                                <strong>Tahap:</strong><br>
+                                1
+                            </li>
+                            <li class="li-kite">
+                                <i class="fas fa-envelope top-6"></i>
+                                <strong>Email:</strong><br>
+                                <a href="mailto:?">yeap@jupem.gov.my</a>
+                            </li>
+                            <li class="li-kite">
+                                <i class="fas fa-phone top-6"></i>
+                                <strong>Tel:</strong><br>
+                                03-2617 0703
+                            </li>
+                        </ul>
+                    </span>
+                </span>
+            </div>
+            <div class="col-sm-6 col-lg-6 mb-4 mb-lg-0">
+                <span class="thumb-info thumb-info-hide-wrapper-bg thumb-info-no-zoom">
+                    <span class="thumb-info-wrapper">
+                        <a href="#">
+                            <img src="{{ asset('storage/upload/img/pegawai/4b502-encik-fuad.jpg') }}"
+                                class="img-fluid" alt="" style="width: 400px !important">
+                        </a>
+                    </span>
+                    <span class="thumb-info-caption">
+                        <ul class="list list-icons list-icons-style-2 list-icons-sm mt-2 appear-animation ul-kite"
+                            data-appear-animation="maskUp" data-appear-animation-delay="300">
+                            <h4 class="font-weight-bold mb-2 size-text-standard">Sr MOHAMAD FUAD BIN KAHAR</h4>
+                            <h2 class="font-weight-bold mb-0 pt-0 mt-0 size-text-standard-2">
+                                Penolong Pengarah Ukur Seksyen Sempadan Negeri dan Penyelarasan, Bahagian Ehwal
+                                Persempadanan
+                            </h2>
+                            <li class="li-kite">
+                                <i class="fas fa-envelope top-6"></i>
+                                <strong>Gred:</strong><br>
+                                Juruukur Gred J48 (KUP) (SME)
+                            </li>
+                            <li class="li-kite">
+                                <i class="fas fa-envelope top-6"></i>
+                                <strong>Bidang Kepakaran:</strong><br>
+                                SME Persempadanan (Persempadanan Darat)
+                            </li>
+                            <li class="li-kite">
+                                <i class="fas fa-envelope top-6"></i>
+                                <strong>Tahap:</strong><br>
+                                1
+                            </li>
+                            <li class="li-kite">
+                                <i class="fas fa-envelope top-6"></i>
+                                <strong>Email:</strong><br>
+                                <a href="mailto:?">fuad@jupem.gov.my</a>
+                            </li>
+                            <li class="li-kite">
+                                <i class="fas fa-phone top-6"></i>
+                                <strong>Tel:</strong><br>
+                                03-2617 0627
+                            </li>
+                        </ul>
+                    </span>
+                </span>
+            </div>
+            <div class="col-sm-6 col-lg-6 mb-4 mb-lg-0">
+                <span class="thumb-info thumb-info-hide-wrapper-bg thumb-info-no-zoom">
+                    <span class="thumb-info-wrapper">
+                        <a href="#">
+                            <img src="{{ asset('storage/upload/img/pegawai/57dde-sr-muhammad-asyran-bin-che-amat-.jpg') }}"
+                                class="img-fluid" alt="" style="width: 400px !important">
+                        </a>
+                    </span>
+                    <span class="thumb-info-caption">
+                        <ul class="list list-icons list-icons-style-2 list-icons-sm mt-2 appear-animation ul-kite"
+                            data-appear-animation="maskUp" data-appear-animation-delay="300">
+                            <h4 class="font-weight-bold mb-2 size-text-standard">Sr DR. MUHAMMAD ASYRAN BIN CHE AMAT
+                            </h4>
+                            <h2 class="font-weight-bold mb-0 pt-0 mt-0 size-text-standard-2">
+                                Penolong Pengarah Ukur - SME Geodetik (Satelit Geodetik (GPS))
+                            </h2>
+                            <li class="li-kite">
+                                <i class="fas fa-envelope top-6"></i>
+                                <strong>Gred:</strong><br>
+                                Juruukur Gred J48 (KUP) (SME)
+                            </li>
+                            <li class="li-kite">
+                                <i class="fas fa-envelope top-6"></i>
+                                <strong>Bidang Kepakaran:</strong><br>
+                                SME Persempadanan (Persempadanan Darat)
+                            </li>
+                            <li class="li-kite">
+                                <i class="fas fa-envelope top-6"></i>
+                                <strong>Tahap:</strong><br>
+                                1
+                            </li>
+                            <li class="li-kite">
+                                <i class="fas fa-envelope top-6"></i>
+                                <strong>Email:</strong><br>
+                                <a href="mailto:?">asyran@jupem.gov.my</a>
+                            </li>
+                            <li class="li-kite">
+                                <i class="fas fa-phone top-6"></i>
+                                <strong>Tel:</strong><br>
+                                03-2617 0928
                             </li>
                         </ul>
                     </span>
